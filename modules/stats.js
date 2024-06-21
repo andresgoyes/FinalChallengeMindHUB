abf04be956e491c3f848c38b152234d5b265d192
metlel32 <metlel32@gmail.com>
2024-06-21 11:40:08 -0500

    tablas de stats casi terminadas

--- modules/stats.js
+++ modules/stats.js
@@ -1,81 +1,40 @@
-const urlApi = "https://aulamindhub.github.io/amazing-api/events.json";
+let categoriasBebidasCotel = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+let categoriasBebidas= "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink"
+let categoriasComida = "https://www.themealdb.com/api/json/v1/1/categories.php"
+
 const { createApp } = Vue;
 
-createApp({
-    data() {
-        return {
-            events: [],
-            currentDate: '',
-            upcomingEvents: [],
-            pastEvents: []
-        };
-    },
-    created() {
-        this.fetchData(urlApi);
-    },
-    computed: {
-        highestAssistance() {
-            return this.calculateHighestAssistance(this.pastEvents);
-        },
-        lowestAssistance() {
-            return this.calculateLowestAssistance(this.pastEvents);
-        },
-        highestCapacity() {
-            return this.calculateHighestCapacity(this.events);
-        },
-        upcomingStats() {
-            return this.calculateEventStatistics(this.upcomingEvents);
-        },
-        pastStats() {
-            return this.calculateEventStatistics(this.pastEvents);
+const app = createApp({
+    data(){
+        return{
+            categorias:[],
+            categoriasBebidas:[],
+            categoriasBebidasCoctel:[]
         }
     },
-    methods: {
-        fetchData(url) {
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    this.events = data.events;
-                    this.currentDate = data.currentDate;
+    created(){
+        this.traerData(categoriasComida)
+        this.traerData2(categoriasBebidas)
+        this.traerdata3(categoriasBebidasCotel)
+    },
+    methods:{
+        traerData(url){
+            fetch(url).then(response => response.json()).then(data => {
+                this.categorias = data.categories
+            })
+        },traerData2(url){
+            fetch(url).then(response => response.json()).then(data => {
+                this.categoriasBebidas = data.drinks
+            })
+        },traerdata3(url){
+            fetch(url).then(response => response.json()).then(data => {
 
-                    this.upcomingEvents = this.events.filter(event => new Date(event.date) > new Date(this.currentDate));
-                    this.pastEvents = this.events.filter(event => new Date(event.date) < new Date(this.currentDate));
-                });
-        },
-        calculateHighestAssistance(events) {
-            let highestAssistanceEvent = events.reduce((max, event) => {
-                const percentage = (event.assistance / event.capacity) * 100;
-                return percentage > max.percentage ? { name: event.name, percentage: percentage.toFixed(2) } : max;
-            }, { name: '', percentage: 0 });
-            return highestAssistanceEvent;
-        },
-        calculateLowestAssistance(events) {
-            let lowestAssistanceEvent = events.reduce((min, event) => {
-                const percentage = (event.assistance / event.capacity) * 100;
-                return percentage < min.percentage ? { name: event.name, percentage: percentage.toFixed(2) } : min;
-            }, { name: '', percentage: 100 });
-            return lowestAssistanceEvent;
-        },
-        calculateHighestCapacity(events) {
-            let highestCapacityEvent = events.reduce((max, event) => {
-                return event.capacity > max.capacity ? { name: event.name, capacity: event.capacity } : max;
-            }, { name: '', capacity: 0 });
-            return highestCapacityEvent;
-        },
-        calculateEventStatistics(events) {
-            const categories = [...new Set(events.map(event => event.category))];
-            return categories.map(category => {
-                const categoryEvents = events.filter(event => event.category === category);
-                const totalRevenues = categoryEvents.reduce((sum, event) => sum + (event.price * (event.assistance || event.estimate)), 0);
-                const totalAssistance = categoryEvents.reduce((sum, event) => sum + (event.assistance || event.estimate), 0);
-                const totalCapacity = categoryEvents.reduce((sum, event) => sum + event.capacity, 0);
-                const percentageAssistance = (totalAssistance / totalCapacity * 100).toFixed(2);
-                return {
-                    name: category,
-                    revenues: totalRevenues,
-                    percentage: percentageAssistance
-                };
-            });
+                this.categoriasBebidasCoctel = data.drinks
+                console.log(this.categoriasBebidasCoctel);
+            })
         }
     }
-}).mount('#app');
+
+
+
+}).mount('#app')
